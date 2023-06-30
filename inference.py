c94253965b0fc5d5d13d7929badbfe10bf5cbfb7
siliconbay <ioioioio10@naver.com>
2023-06-30 13:49:21 +0900

    Add files via upload

--- inference.py
+++ inference.py
@@ -46,28 +46,32 @@ def choice_device(device_type: str) -> torch.device:
 def build_model(device: torch.device) -> nn.Module:
     # Initialize the super-resolution model
     espcn = model.__dict__["ESPCN"](in_channels = 1, out_channels = 1, channels = 64, upscale_factor = 4)
-    g_model = model.__dict__["SRResNet"](1, 1 , 64, 16, 1)
-
+    fsrcnn = model.__dict__["FSRCNN"](upscale_factor=4)
+    g_model = model.__dict__["UNet"](config.in_channels, config.out_channels)
+    
     espcn = espcn.to(device)
+    fsrcnn = espcn.to(device)
     g_model = g_model.to(device)
 
-    return espcn, g_model
+    return espcn, fsrcnn, g_model
 
 
 def main(args):
     device = choice_device(args.device_type)
 
     # Initialize the model
-    espcn_model, g_model = build_model(device)
+    espcn_model, fsrcnn_model, g_model = build_model(device)
     print(f"Build `{args.model_arch_name}` model successfully.")
 
     # Load model weights
-    espcn_model = load_state_dict(espcn_model, args.pretrained_model_weights_path)
+    espcn_model = load_state_dict(espcn_model, args.espcn_model_weights_path)
+    fsrcnn_model = load_state_dict(espcn_model, args.fsrcnn_model_weights_path)
     g_model = load_state_dict(g_model, args.model_weights_path)
     print(f"Load `{args.model_arch_name}` model weights `{os.path.abspath(args.model_weights_path)}` successfully.")
 
     # Start the verification mode of the model.
     espcn_model.eval()
+    fsrcnn_model.eval()
     g_model.eval()
 
     lr_y_tensor, lr_cb_image, lr_cr_image = imgproc.preprocess_one_image(args.inputs_path, device)
@@ -84,7 +88,11 @@ def main(args):
     
     print("lr_y_tensor.shape=", lr_y_tensor.shape)
     with torch.no_grad():
-        sr_y_tensor = g_model(espcn_model(lr_y_tensor))
+        sr_1 = espcn_model(lr_y_tensor.detach().clone())
+        sr_2 = fsrcnn_model(lr_y_tensor.detach().clone())
+        map = torch.cat((sr_1, sr_2), 1)
+        hypo = g_model(map)
+        sr_y_tensor = sr_1 * hypo + sr_2 * (1 - hypo)
     print("sr_y_tensor.shape=", sr_y_tensor.shape)
     
     # Save image
@@ -95,6 +103,24 @@ def main(args):
     sr_ycbcr_image = cv2.merge([sr_y_image[:, :, 0], bic_cb_image, bic_cr_image])
     sr_image = imgproc.ycbcr_to_bgr(sr_ycbcr_image)
     cv2.imwrite(args.output_path, sr_image * 255.0)
+
+    print(f"SR image save to `{args.output_path}`")
+
+    sr_y_image = imgproc.tensor_to_image(sr_1, range_norm=False, half=False)
+    sr_y_image = sr_y_image.astype(np.float32) / 255.0
+
+    sr_ycbcr_image = cv2.merge([sr_y_image[:, :, 0], bic_cb_image, bic_cr_image])
+    sr_image = imgproc.ycbcr_to_bgr(sr_ycbcr_image)
+    cv2.imwrite(args.output_espcn_path, sr_image * 255.0)
+
+    print(f"SR image save to `{args.output_path}`")
+    
+    sr_y_image = imgproc.tensor_to_image(sr_2, range_norm=False, half=False)
+    sr_y_image = sr_y_image.astype(np.float32) / 255.0
+
+    sr_ycbcr_image = cv2.merge([sr_y_image[:, :, 0], bic_cb_image, bic_cr_image])
+    sr_image = imgproc.ycbcr_to_bgr(sr_ycbcr_image)
+    cv2.imwrite(args.output_fsrcnn_path, sr_image * 255.0)
 
     print(f"SR image save to `{args.output_path}`")
 
@@ -115,13 +141,25 @@ if __name__ == "__main__":
                         type=str,
                         default="./figure/sr_comic.png",
                         help="Super-resolution image path.")
+    parser.add_argument("--output_espcn_path",
+                        type=str,
+                        default="./figure/sr_espcn_comic.png",
+                        help="Super-resolution image path.")
+    parser.add_argument("--output_fsrcnn_path",
+                        type=str,
+                        default="./figure/sr_fsrcnn_comic.png",
+                        help="Super-resolution image path.")
     parser.add_argument("--model_weights_path",
                         type=str,
                         default="./samples/ensemble_x4/g_epoch_30.pth.tar",
                         help="Model weights file path.")
-    parser.add_argument("--pretrained_model_weights_path",
+    parser.add_argument("--espcn_model_weights_path",
                         type=str,
                         default="./pretrained/ESPCN_x4-T91-64bf5ee4.pth.tar",
+                        help="Model weights file path.")
+    parser.add_argument("--fsrcnn_model_weights_path",
+                        type=str,
+                        default="./pretrained/fsrcnn_x4-T91-97a30bfb.pth.tar",
                         help="Model weights file path.")
     parser.add_argument("--device_type",
                         type=str,
